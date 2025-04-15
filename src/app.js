a6236511bceb608a1a2a2c24f36384cff52f36e4
Allysson Ryan <allyssonr002@gmail.com>
2025-04-15 10:10:47 -0300

    update cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 connectDB();
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
